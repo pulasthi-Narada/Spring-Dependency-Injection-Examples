a66edfd9baa5fb54f7a2f1c13c5c671e55264b02
Pulasthi Narada <pulasthinarada@hotmail.com>
2022-10-01 10:38:43 +0530

    ConfigurableApplicationContext

--- src/main/java/lk/pulasthi/sfgdi/SfgDiApplication.java
+++ src/main/java/lk/pulasthi/sfgdi/SfgDiApplication.java
@@ -1,13 +1,20 @@
 package lk.pulasthi.sfgdi;
 
+import lk.pulasthi.sfgdi.controllers.Controller;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class SfgDiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SfgDiApplication.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(SfgDiApplication.class, args);
+		  Controller controller = (Controller) context.getBean("controller");
+
+		System.out.println(controller.hello());
 	}
 
 }
--- src/main/java/lk/pulasthi/sfgdi/controllers/Controller.java
+++ src/main/java/lk/pulasthi/sfgdi/controllers/Controller.java
@@ -1,2 +1,10 @@
-package lk.pulasthi.sfgdi.controllers;public class Controller {
+package lk.pulasthi.sfgdi.controllers;
+
+@org.springframework.stereotype.Controller
+public class Controller {
+
+    public String hello(){
+        System.out.println("Hello world");
+        return "hello";
+    }
 }
