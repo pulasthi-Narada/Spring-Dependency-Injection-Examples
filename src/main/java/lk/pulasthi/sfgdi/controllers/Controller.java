3a67e320db19c45bcd861c4c38da6455cd04a16c
Pulasthi Narada <pulasthinarada@hotmail.com>
2022-10-01 14:30:06 +0530

    using  @Primary

--- src/main/java/lk/pulasthi/sfgdi/controllers/Controller.java
+++ src/main/java/lk/pulasthi/sfgdi/controllers/Controller.java
@@ -1,10 +1,17 @@
 package lk.pulasthi.sfgdi.controllers;
 
+import lk.pulasthi.sfgdi.services.GreetingService;
+
 @org.springframework.stereotype.Controller
 public class Controller {
+   private final GreetingService greetingService;
+
+    public Controller(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
 
     public String hello(){
-        System.out.println("Hello world");
-        return "hello";
+
+        return greetingService.sayGreeting();
     }
 }
---
+++ src/main/java/lk/pulasthi/sfgdi/services/PrimaryGreetingService.java
@@ -0,0 +1,14 @@
+package lk.pulasthi.sfgdi.services;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+@Primary
+@Service
+public class PrimaryGreetingService implements GreetingService{
+
+    @Override
+    public String sayGreeting() {
+        return "Hello world -Primary";
+    }
+}
